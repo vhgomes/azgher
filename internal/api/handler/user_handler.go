c2e8e2156083df5dbf814bb9f486f6ffd00db873
Victor Hugo <vhclaudiogomes@gmail.com>
2026-07-06 17:03:25 -0300

    feat: basic strucuture handler and service

--- internal/api/handler/user_handler.go
+++ internal/api/handler/user_handler.go
@@ -1,1 +1,20 @@
-package khalmyr
+package handler
+
+import (
+	"github.com/gofiber/fiber/v3"
+	"github.com/vhgomes/azgher/internal/service"
+)
+
+type UserHandler struct {
+	UserService *service.UserService
+}
+
+func NewUserHandler(userService *service.UserService) *UserHandler {
+	return &UserHandler{
+		UserService: userService,
+	}
+}
+
+func (h *UserHandler) Create(fiberCtx *fiber.Ctx) {
+
+}
--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -1,1 +1,41 @@
-package khalmyr
+package service
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/vhgomes/azgher/internal/domain"
+	"github.com/vhgomes/azgher/internal/repository"
+)
+
+type UserService struct {
+	repo *repository.UserRepo
+}
+
+func NewUserService(repo *repository.UserRepo) *UserService {
+	return &UserService{repo: repo}
+}
+
+func (s *UserService) Create(ctx context.Context, user *domain.User) (*domain.User, error) {
+
+}
+
+func (s *UserService) ById(ctx context.Context, id int) (*domain.User, error) {
+
+}
+
+func (s *UserService) ByEmail(ctx context.Context, email string) (*domain.User, error) {
+
+}
+
+func (s *UserService) ByGoogleID(ctx context.Context, googleID string) (*domain.User, error) {
+
+}
+
+func (s *UserService) Update(ctx context.Context, user *domain.User) (*domain.User, error) {
+
+}
+
+func (s *UserService) SoftDelete(ctx context.Context, id uuid.UUID) error {
+
+}
