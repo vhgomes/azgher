a518f558328ba5ef8c5e569fa13dbf355a776d5c
Victor Hugo <vhclaudiogomes@gmail.com>
2026-07-07 11:14:04 -0300

    feat: service creating user added

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -4,8 +4,13 @@ import (
 	"context"
 
 	"github.com/google/uuid"
+	"github.com/vhgomes/azgher/internal/api/dto"
 	"github.com/vhgomes/azgher/internal/domain"
 	"github.com/vhgomes/azgher/internal/repository"
+	"github.com/vhgomes/azgher/pkg/logger"
+	"go.uber.org/zap"
+
+	"github.com/vhgomes/azgher/pkg/errors"
 )
 
 type UserService struct {
@@ -16,23 +21,49 @@ func NewUserService(repo *repository.UserRepo) *UserService {
 	return &UserService{repo: repo}
 }
 
-func (s *UserService) Create(ctx context.Context, user *domain.User) (*domain.User, error) {
+func (s *UserService) Create(ctx context.Context, dto dto.CreateUserRequest) error {
+	logger.Info("verifing if %s is already registered", zap.String("email", dto.Email))
+	userExists, err := s.repo.ByEmail(ctx, dto.Email)
+
+	if err != nil {
+		logger.Error("failed to verify email", err)
+		return err
+	}
+
+	if userExists != nil {
+		logger.Info("email already registered")
+		return errors.ErrEmailAlreadyRegistered
+	}
+
+	user := dto.ToDomain()
+
+	logger.Info("creating user", zap.String("name", user.Name), zap.String("email", user.Email))
+	err = s.repo.Create(ctx, user)
+
+	if err != nil {
+		logger.Error("failed to create user", err)
+		return err
+	}
+
+	return nil
+}
+
+// TODO: Retornar DTO
+func (s *UserService) ById(ctx context.Context, id int) error {
 
 }
 
-func (s *UserService) ById(ctx context.Context, id int) (*domain.User, error) {
+// TODO: Retornar DTO
+func (s *UserService) ByEmail(ctx context.Context, email string) error {
 
 }
 
-func (s *UserService) ByEmail(ctx context.Context, email string) (*domain.User, error) {
+// TODO: Retornar DTO
+func (s *UserService) ByGoogleID(ctx context.Context, googleID string) error {
 
 }
 
-func (s *UserService) ByGoogleID(ctx context.Context, googleID string) (*domain.User, error) {
-
-}
-
-func (s *UserService) Update(ctx context.Context, user *domain.User) (*domain.User, error) {
+func (s *UserService) Update(ctx context.Context, user *domain.User) error {
 
 }
 
