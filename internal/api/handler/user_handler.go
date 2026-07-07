f7109ee10436c4a5a2b978d3ca5cea332b356c3f
Victor Hugo <vhclaudiogomes@gmail.com>
2026-07-07 11:14:13 -0300

    feat: handler create user added

--- internal/api/handler/user_handler.go
+++ internal/api/handler/user_handler.go
@@ -2,7 +2,9 @@ package handler
 
 import (
 	"github.com/gofiber/fiber/v3"
+	"github.com/vhgomes/azgher/internal/api/dto"
 	"github.com/vhgomes/azgher/internal/service"
+	"github.com/vhgomes/azgher/pkg/validator"
 )
 
 type UserHandler struct {
@@ -15,6 +17,27 @@ func NewUserHandler(userService *service.UserService) *UserHandler {
 	}
 }
 
-func (h *UserHandler) Create(fiberCtx *fiber.Ctx) {
+func (h *UserHandler) Create(c fiber.Ctx) error {
+	var req dto.CreateUserRequest
+	if err := c.Bind().Body(&req); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "invalid payload",
+		})
+	}
 
+	if validationErrs := validator.Validate(&req); validationErrs != nil {
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
+			"errors": validationErrs,
+		})
+	}
+
+	err := h.UserService.Create(c.Context(), req)
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "error creating user",
+		})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{"message": "user created"})
 }
